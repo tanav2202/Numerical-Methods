70ecf540387fa9abb2c00d56da2ca5c6a4911c15
Umang Bhalla <67634565+umgbhalla@users.noreply.github.com>
2021-09-24 03:04:49 +0530

    Newtons Raphson completed

--- Newton-Raphson_Method.py
+++ Newton-Raphson_Method.py
@@ -2,32 +2,23 @@ from math import *
 from sympy import symbols,diff
 from sympy.abc import x,y
 
-
-# def standardNR():
-#     print ("standard nr")
-#     f = input("enter f(x) in pyth n syntax,ie. x^2 as x**2 , write math functions as func() , like exp(), tan().\n \n")
-
 def standardNR():
     print("standard nr")
-    f = input("enter f(x) in pyth n syntax,ie. x^2 as x**2 , write math  functions as func() , like exp(), tan().\n \n")
+    f = input("enter f(x) in pyth n syntax,ie. x^2 as x**2 , write math  functions as func() , like exp(), tan().\n")
     fx = diff(f, x)
     lf = eval("lambda x:"+str(f))
     lfx = eval("lambda x:"+str(fx))
     x_0 = float(input("x0 :"))
     h=lambda x: x - lf(x)/lfx(x)
-
     print ("n \t x1  \t\t x2  ")
     counter = 1
     while (abs (( x_0-h(x_0))) >= 0.00001) :
-
         hv = h(x_0)
         print(counter,"\t",'%.5f'%x_0,"\t",'%.5f'%hv,"\t")
         counter +=1
         x_0=h(x_0)
 
-
     print("The value of root is : ",'%.5f'%x_0,)
-
 
 def non_HomoNR():
     f = input("enter f(x,y) in pyth n syntax,ie. x^2 as x**2 , write math functions as func() , like exp(), tan(). \n")
@@ -45,18 +36,14 @@ def non_HomoNR():
     lfy= eval("lambda x,y:"+str(fy))
     lgy= eval("lambda x,y:"+str(gy))
     counter = 0
-    # print("n\t    a\t\t  f(a)\t\t  b\t"+"  "+"\tf(b)\t\t  c=(a+b)/2\t  f(c)\t\tupdate")
 
-    # print(fx,gx,fy,gy)
     h = lambda x,y: ((lg(x,y)*lfy(x,y)-lf(x,y)*lgy(x,y))/\
             (lfx(x,y)*lgy(x,y)-lgx(x,y)*lfy(x,y)))
     k = lambda x,y: ((lf(x,y)*lgx(x,y)-lg(x,y)*lfx(x,y))/\
             (lfx(x,y)*lgy(x,y)-lgx(x,y)*lfy(x,y)))
 
-    # print(h(x_0,y_0),k(x_0,y_0),f , g)
     print ("n \t x \t\t y \t\t h \t\t k ")
     while (abs((h(x_0,y_0)+k(x_0,y_0))) >= 0.00002):
-
         hv = h(x_0,y_0)
         kv = k(x_0,y_0)
         print(counter,"\t",'%.5f'%x_0,"\t",'%.5f'%y_0,"\t" ,'%.5f'%hv,"\t" , '%.5f'%kv)
@@ -64,26 +51,20 @@ def non_HomoNR():
         x_0=x_0+h(x_0,y_0)
         y_0=y_0+k(x_0,y_0)
 
-
-
     print("The value of root is : ",'%.5f'%x_0,'%.5f'%y_0)
-
-
 
 
 
 def main():
     print("Starting.........")
     # a , b  = map(int, input("enter a b :").split())
-    # case=input("enter 1 for single variable(does not work) and 2 for multi \t")
-    # if case == 1:
-    #     standardNR()
-    # else :
-    #     non_HomoNR()
+    case=int(input("enter 1 for single variable and 2 for multi \t"))
 
-    standardNR()
+    if case == 1:
+        standardNR()
+    else :
+        non_HomoNR()
 
-    # bisection(a, b)
 
 if __name__ == "__main__":
     main()
